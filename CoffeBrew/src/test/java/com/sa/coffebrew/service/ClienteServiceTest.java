031f86d17659a94f5feb05c0a42b720bf348bc3e
JOÃO CARLOS MARTINS CUNHA <joao_c_cunha@estudante.sesisenai.org.br>
2024-05-29 17:32:31 -0300

    sem text

--- CoffeBrew/src/test/java/com/sa/coffebrew/service/ClienteServiceTest.java
+++ CoffeBrew/src/test/java/com/sa/coffebrew/service/ClienteServiceTest.java
@@ -1,99 +1,99 @@
-package com.sa.coffebrew.service;
-
-import com.sa.coffebrew.entity.Cliente;
-import com.sa.coffebrew.repository.ClienteRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-import java.util.Optional;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
-
-@SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class ClienteServiceTest {
-
-    @Autowired
-    private ClienteService clienteService;
-
-    @Autowired
-    private ClienteRepository clienteRepository;
-
-    private Cliente cliente;
-    
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-
-    @BeforeEach
-    void setUp() {
-        cliente = new Cliente();
-        cliente.setCelular(123456789L);
-        cliente.setFone(987654321L);
-        cliente.setEmail("teste@exemplo.com");
-        clienteRepository.save(cliente);
-    }
-    @AfterEach
-    public void tearDown() {
-    }
-
-    @Test
-    @Order(1)
-    void testIncluirCliente() {
-        Cliente novoCliente = new Cliente();
-        novoCliente.setCelular(123456780L);
-        novoCliente.setFone(987654320L);
-        novoCliente.setEmail("a@exemplo.com");
-
-        Long idCliente = clienteService.incluirCliente(novoCliente);
-        assertNotNull(idCliente);
-
-    }
-
-    @Test
-    @Order(2)
-    void testConsultarCliente() {
-        Optional<Cliente> result = clienteService.consultarCliente(cliente.getIdCliente());
-        System.out.println(cliente.getIdCliente());
-        assertTrue(result.isPresent());
-    }
-    
-
-    @Test
-    @Order(3)
-    void testListarClientes() {
-        List<Cliente> result = clienteService.listarClientes();
-        System.out.println(result);
-        assertFalse(result.isEmpty());
-    }
-
-    @Test
-    @Order(4)
-    void testAtualizarCliente() {
-        cliente.setEmail("updated@example.com");
-        Boolean result = clienteService.atualizarCliente(cliente);
-        assertTrue(result);
-    }
-    
-    @Test
-    @Order(5)
-    void testExcluirCliente() {
-        
-        Boolean result = clienteService.excluirCliente(cliente.getIdCliente());
-        assertTrue(result);
-    }
-}
+//package com.sa.coffebrew.service;
+//
+//import com.sa.coffebrew.entity.Cliente;
+//import com.sa.coffebrew.repository.ClienteRepository;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//import java.util.Optional;
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.AfterEach;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.MethodOrderer;
+//import org.junit.jupiter.api.Order;
+//import org.junit.jupiter.api.TestMethodOrder;
+//
+//@SpringBootTest
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//public class ClienteServiceTest {
+//
+//    @Autowired
+//    private ClienteService clienteService;
+//
+//    @Autowired
+//    private ClienteRepository clienteRepository;
+//
+//    private Cliente cliente;
+//    
+//    
+//    @BeforeAll
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterAll
+//    public static void tearDownClass() {
+//    }
+//
+//    @BeforeEach
+//    void setUp() {
+//        cliente = new Cliente();
+//        cliente.setCelular(123456789L);
+//        cliente.setFone(987654321L);
+//        cliente.setEmail("teste@exemplo.com");
+//        clienteRepository.save(cliente);
+//    }
+//    @AfterEach
+//    public void tearDown() {
+//    }
+//
+//    @Test
+//    @Order(1)
+//    void testIncluirCliente() {
+//        Cliente novoCliente = new Cliente();
+//        novoCliente.setCelular(123456780L);
+//        novoCliente.setFone(987654320L);
+//        novoCliente.setEmail("a@exemplo.com");
+//
+//        Long idCliente = clienteService.incluirCliente(novoCliente);
+//        assertNotNull(idCliente);
+//
+//    }
+//
+//    @Test
+//    @Order(2)
+//    void testConsultarCliente() {
+//        Optional<Cliente> result = clienteService.consultarCliente(cliente.getIdCliente());
+//        System.out.println(cliente.getIdCliente());
+//        assertTrue(result.isPresent());
+//    }
+//    
+//
+//    @Test
+//    @Order(3)
+//    void testListarClientes() {
+//        List<Cliente> result = clienteService.listarClientes();
+//        System.out.println(result);
+//        assertFalse(result.isEmpty());
+//    }
+//
+//    @Test
+//    @Order(4)
+//    void testAtualizarCliente() {
+//        cliente.setEmail("updated@example.com");
+//        Boolean result = clienteService.atualizarCliente(cliente);
+//        assertTrue(result);
+//    }
+//    
+//    @Test
+//    @Order(5)
+//    void testExcluirCliente() {
+//        
+//        Boolean result = clienteService.excluirCliente(cliente.getIdCliente());
+//        assertTrue(result);
+//    }
+//}
--- CoffeBrew/src/test/java/com/sa/coffebrew/service/FuncionarioServiceTest.java
+++ CoffeBrew/src/test/java/com/sa/coffebrew/service/FuncionarioServiceTest.java
@@ -1,83 +1,83 @@
-package com.sa.coffebrew.service;
-
-import com.sa.coffebrew.entity.Funcionario;
-import com.sa.coffebrew.repository.FuncionarioRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
-
-@SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class FuncionarioServiceTest {
-
-    @Autowired
-    private FuncionarioService funcionarioService;
-
-    @Autowired
-    private FuncionarioRepository funcionarioRepository;
-
-    private Funcionario funcionario;
-
-    @BeforeEach
-    void setUp() {
-        funcionario = new Funcionario();
-        funcionario.setPerfil("gerente");
-        funcionario.setNome("John Doe");
-        funcionario.setCpf("12345678900");
-        funcionario.setEmail("john.doe@example.com");
-        funcionarioRepository.save(funcionario);
-    }
-
-    @Test
-    @Order(1)
-    void testIncluirFuncionario() {
-        Funcionario newFuncionario = new Funcionario();
-        newFuncionario.setPerfil("gerente");
-        newFuncionario.setNome("Jane Doe");
-        newFuncionario.setCpf("09876543210");
-        newFuncionario.setEmail("jane.doe@example.com");
-
-        Long idFuncionario = funcionarioService.incluirFuncionario(newFuncionario);
-        assertNotNull(idFuncionario);
-
-    }
-
-    @Test
-    @Order(2)
-    void testExcluirFuncionario() {
-        Boolean result = funcionarioService.excluirFuncionario(funcionario.getIdFuncionario());
-        assertTrue(result);
-
-        
-    }
-
-    @Test
-    @Order(3)
-    void testConsultarFuncionario() {
-        Optional<Funcionario> result = funcionarioService.consultarFuncionario(funcionario.getIdFuncionario());
-        assertTrue(result.isPresent());
-    }
-
-    @Test
-    @Order(4)
-    void testListarFuncionarios() {
-        List<Funcionario> result = funcionarioService.listarFuncionarios();
-        assertFalse(result.isEmpty());
-    }
-
-    @Test
-    @Order(5)
-    void testAtualizarFuncionario() {
-        funcionario.setEmail("updated.john.doe@example.com");
-        Boolean result = funcionarioService.atualizarFuncionario(funcionario);
-        assertTrue(result);
-    }
-}
+//package com.sa.coffebrew.service;
+//
+//import com.sa.coffebrew.entity.Funcionario;
+//import com.sa.coffebrew.repository.FuncionarioRepository;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import java.util.List;
+//import java.util.Optional;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.MethodOrderer;
+//import org.junit.jupiter.api.Order;
+//import org.junit.jupiter.api.TestMethodOrder;
+//
+//@SpringBootTest
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//class FuncionarioServiceTest {
+//
+//    @Autowired
+//    private FuncionarioService funcionarioService;
+//
+//    @Autowired
+//    private FuncionarioRepository funcionarioRepository;
+//
+//    private Funcionario funcionario;
+//
+//    @BeforeEach
+//    void setUp() {
+//        funcionario = new Funcionario();
+//        funcionario.setPerfil("gerente");
+//        funcionario.setNome("John Doe");
+//        funcionario.setCpf("12345678900");
+//        funcionario.setEmail("john.doe@example.com");
+//        funcionarioRepository.save(funcionario);
+//    }
+//
+//    @Test
+//    @Order(1)
+//    void testIncluirFuncionario() {
+//        Funcionario newFuncionario = new Funcionario();
+//        newFuncionario.setPerfil("gerente");
+//        newFuncionario.setNome("Jane Doe");
+//        newFuncionario.setCpf("09876543210");
+//        newFuncionario.setEmail("jane.doe@example.com");
+//
+//        Long idFuncionario = funcionarioService.incluirFuncionario(newFuncionario);
+//        assertNotNull(idFuncionario);
+//
+//    }
+//
+//    @Test
+//    @Order(2)
+//    void testExcluirFuncionario() {
+//        Boolean result = funcionarioService.excluirFuncionario(funcionario.getIdFuncionario());
+//        assertTrue(result);
+//
+//        
+//    }
+//
+//    @Test
+//    @Order(3)
+//    void testConsultarFuncionario() {
+//        Optional<Funcionario> result = funcionarioService.consultarFuncionario(funcionario.getIdFuncionario());
+//        assertTrue(result.isPresent());
+//    }
+//
+//    @Test
+//    @Order(4)
+//    void testListarFuncionarios() {
+//        List<Funcionario> result = funcionarioService.listarFuncionarios();
+//        assertFalse(result.isEmpty());
+//    }
+//
+//    @Test
+//    @Order(5)
+//    void testAtualizarFuncionario() {
+//        funcionario.setEmail("updated.john.doe@example.com");
+//        Boolean result = funcionarioService.atualizarFuncionario(funcionario);
+//        assertTrue(result);
+//    }
+//}
