982236ad3717b8f00f14668f7bdadaa5fb127b3f
JOÃO CARLOS MARTINS CUNHA <joao_c_cunha@estudante.sesisenai.org.br>
2024-05-29 17:05:41 -0300

    deletar back

--- coffeeBrewBackend/CoffeBrew/src/main/java/com/sa/coffebrew/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sa.coffebrew.repository;
-
-import com.sa.coffebrew.entity.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
